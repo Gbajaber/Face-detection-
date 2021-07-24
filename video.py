c1b6689504366bdc077baac30103b8dc184de2f2
Gbajaber <85759827+Gbajaber@users.noreply.github.com>
2021-07-25 00:15:26 +0300

    Update video.py

--- video.py
+++ video.py
@@ -1,24 +1,18 @@
 import cv2
-# تم استدعاء مكتبه cv2
 # Load the cascade
-#  cascade تم تحميل 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# هذا الامر لالتقاط ابعاد الوجه في الفيديو 
 cap = cv2.VideoCapture(0)
 # To use a video file as input 
 # cap = cv2.VideoCapture('filename.mp4')
-# هنا تم وضع شرط للتحقق
 while True:
     # Read the frame
     _, img = cap.read()
-   # للقراءة
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # امر لالتقاط ابعاد الوجه في الفيديو 
     # Draw the rectangle around each face
 
     for (x, y, w, h) in faces:
@@ -26,7 +20,6 @@ while True:
 
     # Display
     cv2.imshow('img', img)
-    # عرص الصوره الملتقطه من الفيديو
     # Stop if escape key is pressed
     k = cv2.waitKey(30) & 0xff
     if k==27:
